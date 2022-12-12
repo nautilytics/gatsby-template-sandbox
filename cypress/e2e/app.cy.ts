6782d7f4113a11ee7db83e86035b4e93212ef7d1
Chris Lanoue <Chris.lanoue@missionlane.com>
2022-12-12 12:44:47 -0500

    feat: fix tests to work locally

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,13 +1,18 @@
 /* eslint-disable */
 
 describe('Product Offer Templates', () => {
-    it('should navigate to each unique product offer page', () => {
+    it('should navigate to first product offer page', () => {
+
         // Visit and snapshot first product offer page
         cy.visit('http://localhost:8000/product-offers/1/')
+        cy.get('h3').contains('This is the first header in play')
         cy.percySnapshot('first product offer page')
+    })
+    it('should navigate to second product offer page', () => {
 
         // Visit and snapshot second product offer page
         cy.visit('http://localhost:8000/product-offers/2/')
+        cy.get('h3').contains('This is the second header in play')
         cy.percySnapshot('second product offer page')
     })
 })
