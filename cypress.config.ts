517b057dc9b9f374036c10719165dbed29ba6f0d
Chris Lanoue <Chris.lanoue@missionlane.com>
2022-12-12 14:58:03 -0500

    feat add cypress-axe for a11y

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,20 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8000',
-    specPattern: "cypress/e2e"
+    specPattern: "cypress/e2e",
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+
+          return null
+        },
+        table(message) {
+          console.table(message)
+
+          return null
+        }
+      })
+    }
   },
 })
--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,18 +1,43 @@
 /* eslint-disable */
 
+// Define at the top of the spec file or just import it
+function terminalLog(violations) {
+    cy.task(
+        'log',
+        `${violations.length} accessibility violation${
+            violations.length === 1 ? '' : 's'
+        } ${violations.length === 1 ? 'was' : 'were'} detected`
+    )
+    // pluck specific keys to keep the table readable
+    const violationData = violations.map(
+        ({ id, impact, description, nodes }) => ({
+            id,
+            impact,
+            description,
+            nodes: nodes.length
+        })
+    )
+
+    cy.task('table', violationData)
+}
+
 describe('Product Offer Templates', () => {
     it('should navigate to first product offer page', () => {
 
         // Visit and snapshot first product offer page
         cy.visit('http://localhost:8000/product-offers/1/')
+        cy.injectAxe()
         cy.get('h3').contains('This is the first header in play')
+        cy.checkA11y(null, null, terminalLog)
         cy.percySnapshot('first product offer page')
     })
     it('should navigate to second product offer page', () => {
 
         // Visit and snapshot second product offer page
         cy.visit('http://localhost:8000/product-offers/2/')
+        cy.injectAxe()
         cy.get('h3').contains('This is the second header in play')
+        cy.checkA11y(null, null, terminalLog)
         cy.percySnapshot('second product offer page')
     })
 })
